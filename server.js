e157db37f58dc0dfbfd55ac0d3ca32d8d39f33d6
Mark Guinn <mxavier927@gmail.com>
2020-08-22 11:42:35 -0400

    project finished

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use((err, req, res, next)=> {
 const init = async()=> {
   try {
     await db.syncAndSeed();
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 3001;
     app.listen(port, ()=> console.log(`listening on port ${port}`));
   }
   catch(ex){
--- src/index.js
+++ src/index.js
@@ -1,60 +1,69 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Axios = require('axios')
+const Faker = require('faker')
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      friends: []
+    }
+  }
+  async componentDidMount () {
+    try{
+    const response = await Axios.get('api/friends')
+    const friends = response.data;
+    this.setState({friends: friends}) 
+    } catch(err) {console.log(err)}
+  }
+  async createFriend() {
+    const response = await Axios.post('api/friends')
+    const newFriend = response.data
+    const friendArray = this.state.friends;
+    friendArray.push(newFriend);
+    friendArray.sort((a,b) => b.rating - a.rating)
+    this.setState({friends: friendArray}); 
+  }
+  async deleteFriend(id) {
+    await Axios.delete(`api/friends/${id}`)
+    const friends = this.state.friends.filter(friend => friend.id !== id)
+    this.setState({friends: friends})
+  }
+  async ratingChange(id, rating, method) {
+    method === 'plus' ? rating +=1 : rating -=1
+    await Axios.put(`/api/friends/${id}`, { rating: rating })
+    const friendArray = this.state.friends;
+    const friend = friendArray.find(item => item.id === id);
+    friend.rating = rating;
+    friendArray.sort((a,b) => b.rating - a.rating)
+    this.setState({friends: friendArray}); 
+  }
+  render() {
+    return( 
+    <div>
+    <h1>Friends (The List)</h1>
+    <button onClick = {() => this.createFriend()}>Create Friend</button>
+    <div id='error'></div>
+    <ul>
+      {
+      this.state.friends.map(friend => {
+        return ( 
+        <li key={friend.id}>
+        <h2>{ friend.name }</h2>
+        <span>{ friend.rating }</span>
+        <button onClick = {() => this.ratingChange(friend.id, friend.rating, 'plus')}>+</button>
+        <button onClick = {() => this.ratingChange(friend.id, friend.rating, 'minus')}>-</button>
+        <button onClick = {() => this.deleteFriend(friend.id)}>x</button>
+        </li>)
+      })
+      }
+    </ul>
+    </div>
+  )}
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
 const axios = require('axios');
-
-const render = (friends)=> {
-  const ul = document.querySelector('ul');
-  const error = document.querySelector('#error');
-  error.innerText = '';
-  friends.sort((a, b)=> b.rating - a.rating);
-  const html = friends.map( friend => {
-    return `
-      <li data-id='${friend.id}'>
-        <h2>${ friend.name }</h2>
-        <span>${ friend.rating }</span>
-        <button data-id='${friend.id}'>+</button><button data-id='${friend.id}'>-</button><button data-id='${friend.id}'>x</button>
-      </li>
-    `;
-  }).join('');
-  ul.innerHTML = html;
-};
-
-const init = async()=> {
-  const response = await axios.get('/api/friends');
-  let friends = response.data;
-  render(friends);
-  const ul = document.querySelector('ul');
-  const form = document.querySelector('form');
-  const error = document.querySelector('#error');
-
-  ul.addEventListener('click', async(ev)=> {
-    if(ev.target.tagName === 'BUTTON'){
-      if(ev.target.innerHTML === 'x'){
-        const id = ev.target.getAttribute('data-id')*1;
-        await axios.delete(`/api/friends/${id}`); 
-        friends = friends.filter(friend => friend.id !== id); 
-        render(friends);
-      }
-      else {
-        const id = ev.target.getAttribute('data-id')*1;
-        const friend = friends.find(item => item.id === id);
-        const increase = ev.target.innerHTML === '+';
-        friend.rating = increase ? ++friend.rating : --friend.rating;
-        await axios.put(`/api/friends/${friend.id}`, { rating: friend.rating }); 
-        render(friends);
-      }
-    }
-  });
-
-  form.addEventListener('submit', async(ev)=> {
-    ev.preventDefault();
-    try {
-      const response = await axios.post('/api/friends');
-      friends.push(response.data);
-      render(friends);
-    }
-    catch(ex){
-      error.innerText = ex.response.data.error;
-    }
-  });
-};
-
-init();
+const { render } = require('react-dom');
